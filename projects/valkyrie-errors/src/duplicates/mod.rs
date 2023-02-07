ca939ead3e777d7adfdf6fbfdabf13a6263361a1
Aster <galaster@foxmail.com>
2023-02-07 20:42:25 +0800

    Support duplicate kind

--- projects/valkyrie-errors/src/duplicates/mod.rs
+++ projects/valkyrie-errors/src/duplicates/mod.rs
@@ -1,15 +1,23 @@
 use std::{
     fmt::{Debug, Display, Formatter},
-    ops::Range,
 };
 
-use ariadne::{Label, Report, ReportKind};
+use ariadne::{Color, Report, ReportKind};
 
-use crate::{FileID, TextManager, ValkyrieError, ValkyrieErrorKind};
+use crate::{errors::ValkyrieReport, TextSpan, ValkyrieError, ValkyrieErrorKind};
+
+mod kind;
+
+#[derive(Copy, Clone)]
+pub enum DuplicateKind {
+    Type = 1002,
+    Function = 1003,
+    Variable = 1004,
+}
 
 #[derive(Debug)]
 pub struct DuplicateError {
-    king: String,
+    kind: DuplicateKind,
     name: String,
     this_item: TextSpan,
     last_item: TextSpan,
@@ -21,57 +29,26 @@ impl Display for DuplicateError {
     }
 }
 
-pub type ValkyrieReport = Report<(FileID, Range<usize>)>;
-
 impl DuplicateError {
     pub fn as_report(&self, level: ReportKind) -> Result<ValkyrieReport, String> {
-        let mut report = Report::build(level, self.this_item.file, 12).with_code(3);
+        let mut report = Report::build(level, self.this_item.file, 12).with_code(self.kind as u32);
         report.set_message(self.to_string());
-        report.add_label(self.this_item.as_label(format!("This is the first item named `{}`", self.name)));
-        report.add_label(self.last_item.as_label(format!("This is the second item named `{}`", self.name)));
-        report.set_help(format!("Rename one of the items to have a unique name"));
-        report.set_note(format!("Items must have unique names"));
+        report.add_label(
+            self.this_item.as_label(format!("{:?} `{}` is defined here.", self.kind, self.name)).with_color(Color::Blue),
+        );
+        report.add_label(
+            self.last_item
+                .as_label(format!("But {} `{}` has been defined here.", self.kind, self.name))
+                .with_color(Color::Cyan),
+        );
+        report.set_help(format!("Items must have unique names, rename one of the items to have a unique name"));
         Ok(report.finish())
     }
 }
 
 impl ValkyrieError {
-    pub fn as_report(&self) -> Result<ValkyrieReport, String> {
-        match &self.kind {
-            ValkyrieErrorKind::Duplicate(dup) => dup.as_report(self.level),
-        }
-    }
-}
-
-impl ValkyrieError {
     pub fn duplicate_type(name: String, this: TextSpan, last: TextSpan) -> Self {
-        let this = DuplicateError { name, this_item: this, last_item: last };
+        let this = DuplicateError { kind: DuplicateKind::Type, name, this_item: this, last_item: last };
         Self { kind: ValkyrieErrorKind::Duplicate(Box::new(this)), level: ReportKind::Error }
     }
-}
-
-#[test]
-fn main() {
-    use ariadne::{Color, ColorGenerator};
-
-    let mut colors = ColorGenerator::new();
-
-    // Generate & choose some colours for each of our elements
-    let a = colors.next();
-    let b = colors.next();
-    let out = Color::Fixed(81);
-
-    let mut text = TextManager::new("./");
-    let file1 = text.add_file("src/duplicates/mod.rs");
-    let file2 = text.add_file("src/errors/mod.rs");
-
-    ValkyrieError::duplicate_type(
-        "Optional".to_string(),
-        TextSpan { file: file1, head: 32, tail: 33 },
-        TextSpan { file: file2, head: 42, tail: 45 },
-    )
-    .as_report()
-    .unwrap()
-    .print(text)
-    .unwrap();
 }
--- projects/valkyrie-errors/src/errors/mod.rs
+++ projects/valkyrie-errors/src/errors/mod.rs
@@ -1,19 +1,29 @@
 use std::{
-    error::Error,
-    fmt::{Debug, Display},
+    ops::Range,
 };
 
-use ariadne::ReportKind;
+use ariadne::{Report, ReportKind};
 
-use crate::DuplicateError;
+use crate::{DuplicateError, FileID};
 
 pub mod diagnostic;
+
+pub type ValkyrieReport = Report<(FileID, Range<usize>)>;
+pub type ValkyrieResult<T = ()> = Result<T, ValkyrieError>;
+
+pub struct ValkyrieError {
+    pub kind: ValkyrieErrorKind,
+    pub level: ReportKind,
+}
 
 pub enum ValkyrieErrorKind {
     Duplicate(Box<DuplicateError>),
 }
 
-pub struct ValkyrieError {
-    pub kind: ValkyrieErrorKind,
-    pub level: ReportKind,
+impl ValkyrieError {
+    pub fn as_report(&self) -> Result<ValkyrieReport, String> {
+        match &self.kind {
+            ValkyrieErrorKind::Duplicate(e) => e.as_report(self.level),
+        }
+    }
 }
