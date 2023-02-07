f4c83f3c454f1b88a9c4843dd45783efa4dafe89
Aster <galaster@foxmail.com>
2023-02-07 20:42:25 +0800

    Support duplicate kind

--- projects/valkyrie-errors/tests/main.rs
+++ projects/valkyrie-errors/tests/main.rs
@@ -1,23 +1,32 @@
-use miette::{NamedSource, Result};
-use valkyrie_errors::ValkyrieError;
+use valkyrie_errors::{TextManager, TextSpan, ValkyrieError};
 
 #[test]
 fn ready() {
     println!("it works!")
 }
 
-fn this_fails() -> Result<()> {
-    Err(ValkyrieError::duplicate_type("Optional".to_string(), (0, 10), (20, 30)))?;
-    Ok(())
-}
-
-// Now to get everything printed nicely, just return a `Result<()>`
-// and you're all set!
-//
-// Note: You can swap out the default reporter for a custom one using
-// `miette::set_hook()`
 #[test]
-fn pretend_this_is_main() -> Result<()> {
-    this_fails()?;
-    Ok(())
+fn main() {
+    use ariadne::{Color, ColorGenerator};
+
+    let mut colors = ColorGenerator::new();
+
+    // Generate & choose some colours for each of our elements
+    let _a = colors.next();
+    let _b = colors.next();
+    let _out = Color::Fixed(81);
+
+    let mut text = TextManager::new("./");
+    let file1 = text.add_file("src/duplicates/mod.rs");
+    let file2 = text.add_file("src/errors/mod.rs");
+
+    ValkyrieError::duplicate_type(
+        "Optional".to_string(),
+        TextSpan { file: file1, head: 32, tail: 33 },
+        TextSpan { file: file2, head: 42, tail: 45 },
+    )
+    .as_report()
+    .unwrap()
+    .print(text)
+    .unwrap();
 }
