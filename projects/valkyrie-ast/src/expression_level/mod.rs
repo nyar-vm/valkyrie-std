971b42471be8de015fde838c19cbfa1d91c54d67
Aster <galaster@foxmail.com>
2023-02-08 12:18:18 +0800

    🚧 Define the special node

--- projects/valkyrie-ast/src/expression_level/mod.rs
+++ projects/valkyrie-ast/src/expression_level/mod.rs
@@ -9,34 +9,17 @@ mod class_type;
 mod literal_type;
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
+pub enum AtomicExpression {
+    Null,
+    Boolean(bool),
+    Identifier(String),
+    Integer(String),
+    Decimal(String),
+    PureString(String),
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct BinaryExpression {}
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct UnaryExpression {}
-
-#[derive(Clone, Debug, Serialize, Deserialize)]
-pub enum AtomicExpression {
-    Identifier(String),
-    Number(f64),
-    String(String),
-    Boolean(bool),
-    Null,
-}
-
-impl BinaryExpression {
-    pub fn to_node(self, file: FileID, range: &Range<usize>) -> ValkyrieASTNode {
-        ValkyrieASTNode { kind: ValkyrieASTKind::Binary(box self), span: FileSpan { file, head: range.start, tail: range.end } }
-    }
-}
-
-impl UnaryExpression {
-    pub fn to_node(self, file: FileID, range: &Range<usize>) -> ValkyrieASTNode {
-        ValkyrieASTNode { kind: ValkyrieASTKind::Unary(box self), span: FileSpan { file, head: range.start, tail: range.end } }
-    }
-}
-
-impl AtomicExpression {
-    pub fn to_node(self, file: FileID, range: &Range<usize>) -> ValkyrieASTNode {
-        ValkyrieASTNode { kind: ValkyrieASTKind::Atomic(box self), span: FileSpan { file, head: range.start, tail: range.end } }
-    }
-}
