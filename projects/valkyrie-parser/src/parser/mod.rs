4f9071a21078d7ccd8c668687045e8b5fcc355f8
Aster <galaster@foxmail.com>
2023-02-08 11:40:46 +0800

    Resolve namespace kind

--- projects/valkyrie-parser/src/parser/mod.rs
+++ projects/valkyrie-parser/src/parser/mod.rs
@@ -1,15 +1,12 @@
-use std::{
-    fs::read_to_string,
-    ops::Range,
-    path::{Path, PathBuf},
-};
+use std::{ops::Range, path::PathBuf};
 
 use peginator::PegParser;
 
-use valkyrie_errors::{FileID, ParseError, Url, ValkyrieError, ValkyrieResult};
+use valkyrie_ast::{NamespaceDeclare, ValkyrieASTNode};
+use valkyrie_errors::{FileID, ParseError, ValkyrieError, ValkyrieResult};
 
 use crate::{
-    parser::valkyrie::{VkParser, VkStatements},
+    parser::valkyrie::{IdentifierNode, LetStatement, NamespaceDeclareNode, VkParser, VkStatements},
     ValkyrieParser,
 };
 
@@ -22,14 +19,17 @@ pub struct ParseContext {
 }
 
 impl ValkyrieParser {
-    pub fn parse_file(&mut self, file: FileID, text: &str) -> ValkyrieResult<()> {
+    pub fn parse_file(&mut self, file: FileID, text: &str) -> ValkyrieResult<Vec<ValkyrieASTNode>> {
         self.file = file;
         let stmts = match VkParser::parse(text) {
             Ok(o) => o.statements,
             Err(e) => Err(ParseError::from(e).with_file(file))?,
         };
-        self.parse_root(&stmts)?;
-        Ok(())
+        let mut out = vec![];
+        for s in stmts {
+            s.visit(self, &mut out)?;
+        }
+        Ok(out)
     }
     pub fn take_errors(&mut self) -> Vec<ValkyrieError> {
         std::mem::take(&mut self.errors)
@@ -43,12 +43,52 @@ impl ValkyrieParser {
     }
 }
 
-impl ValkyrieParser {
-    pub fn parse_root(&mut self, root: &[VkStatements]) -> ValkyrieResult<()> {
-        for stmt in root {
-            println!("{:#?}", stmt);
+impl VkStatements {
+    pub fn visit(&self, parser: &mut ValkyrieParser, out: &mut Vec<ValkyrieASTNode>) -> ValkyrieResult {
+        match self {
+            VkStatements::Semicolon(_) => {}
+            VkStatements::ControlFlowNode(_) => {
+                todo!()
+            }
+            VkStatements::DefStatement(_) => {
+                todo!()
+            }
+            VkStatements::ExpressionNode(_) => {
+                todo!()
+            }
+            VkStatements::LetStatement(v) => out.push(v.visit(parser)?),
+            VkStatements::LoopStatement(_) => {
+                todo!()
+            }
+            VkStatements::NamespaceDeclareNode(v) => out.push(v.visit(parser)?),
+
+            VkStatements::WhileStatement(_) => {
+                todo!()
+            }
         }
         Ok(())
+    }
+}
+
+impl NamespaceDeclareNode {
+    pub fn visit(&self, parser: &mut ValkyrieParser) -> ValkyrieResult<ValkyrieASTNode> {
+        let mut out = NamespaceDeclare::new(&self.kw);
+        for name in &self.namespace.path {
+            out.push_name(name.get_identifier());
+        }
+        Ok(out.to_node(parser.file, &self.namespace.position))
+    }
+}
+
+impl LetStatement {
+    pub fn visit(&self, parser: &mut ValkyrieParser) -> ValkyrieResult<ValkyrieASTNode> {
+        todo!()
+    }
+}
+
+impl IdentifierNode {
+    pub fn get_identifier(&self) -> String {
+        self.string.to_string()
     }
 }
 
