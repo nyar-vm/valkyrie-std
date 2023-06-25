69cee4821b94457b7c398e4d025582f84c8a4f25
Aster <aster@vers.site>
2023-06-25 20:54:41 +0800

    🚧 Define the list and iterable

--- projects/valkyrie-ast/src/expression_level/binary.rs
+++
@@ -1,7 +0,0 @@
-use super::*;
-
-impl BinaryExpression {
-    pub fn to_node(self, file: FileID, range: &Range<usize>) -> ValkyrieASTNode {
-        ValkyrieASTNode { kind: ValkyrieASTKind::Binary(box self), span: FileSpan { file, head: range.start, tail: range.end } }
-    }
-}
--- projects/valkyrie-ast/tests/main.rs
+++ projects/valkyrie-ast/tests/main.rs
@@ -1,17 +1,4 @@
-use std::path::PathBuf;
-
-use peginator_codegen::Compile;
-
-use valkyrie_errors::{Url, ValkyrieResult};
-
 #[test]
-fn build_parser() -> ValkyrieResult {
-    let root = PathBuf::from("../valkyrie-std-tensor").canonicalize()?;
-    let input = root.join("valkyrie.peg");
-    let output = root.join("src/parser/valkyrie.rs");
-    println!("Project root at: {}", Url::from_file_path(&root)?);
-    println!("Input file at: {}", Url::from_file_path(&input)?);
-    println!("Output file at: {}", Url::from_file_path(&output)?);
-    Compile::file(input).destination(output).format().run_exit_on_error();
-    Ok(())
+fn ready() {
+    println!("It works!");
 }
